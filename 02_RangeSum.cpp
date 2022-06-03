b0fd916c6e01d8119903d3adddf4cd660209903b
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-03 12:21:45 +0530

    Range Sum

--- 02_RangeSum.cpp
+++ 02_RangeSum.cpp
@@ -19,7 +19,33 @@ using namespace std;
 
 class NumMatrix
 {
-    // TLE 
+    vector<vector<int>> dp;
+
+public:
+    NumMatrix(vector<vector<int>> &matrix)
+    {
+        if (matrix.size() == 0 || matrix[0].size() == 0)
+            return;
+        vector<vector<int>> temp(matrix.size() + 1, vector<int>(matrix.size()));
+        dp = temp;
+        for (int r = 0; r < matrix.size(); r++)
+        {
+            for (int c = 0; c < matrix[0].size(); c++)
+            {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
+            }
+        }
+    };
+
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+};
+
+class NumMatrix
+{
+    // TLE
     vector<vector<int>> matrix;
 
 public:
