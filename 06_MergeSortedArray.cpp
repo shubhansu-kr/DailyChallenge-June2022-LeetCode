41d0f1964fe085e6bc62645f59e21ecf077bdb85
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-07 10:21:24 +0530

    Solution 1 : Space optimised

--- 06_MergeSortedArray.cpp
+++ 06_MergeSortedArray.cpp
@@ -17,6 +17,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1 {
+    // Time Complexity : O(m+n) 
+    // Space Complexity : O(1)
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j=n-1, k=m+n-1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }
+            else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >=0) {
+            nums1[k--] = nums2[j--] ;
+        }
+    }
+};
+
 class Solution {
     // Two pointer approach - Intution 
     // Time complextiy: O(m+n) 
