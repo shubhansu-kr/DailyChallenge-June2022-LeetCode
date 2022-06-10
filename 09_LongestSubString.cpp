b57c72a339d4344eae03d45d440ea3dd0184805b
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-10 13:43:29 +0530

    Longest string with no repeating char

---
+++ 09_LongestSubString.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// Given a string s, find the length of the longest substring
+// without repeating characters.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Sliding Window problem
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        vector<int> ind(256, -1);
+        int len = 0, start = -1;
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (ind[s[i]] > start)
+            {
+                start = ind[s[i]];
+            }
+            ind[s[i]] = i;
+            len = max(len, i - start);
+        }
+        return len;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
