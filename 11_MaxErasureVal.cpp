300d8fdb52bad93e806c3bf24774fddaf4d7aec6
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-12 15:18:37 +0530

    Find the max sum of subArr with unique element

--- 11_MaxErasureVal.cpp
+++ 11_MaxErasureVal.cpp
@@ -15,6 +15,30 @@ using namespace std;
 
 class Solution
 {
+    // Trading speed with memory
+public:
+    int maximumUniqueSubarray(vector<int> &nums)
+    {
+        unordered_set<int> seen;
+        int curr_sum = 0, max_sum = 0, l = 0;
+        for (int num : nums)
+        {
+            while (seen.find(num) != seen.end())
+            {
+                curr_sum -= nums[l];
+                seen.erase(nums[l]);
+                ++l;
+            }
+            curr_sum += num;
+            seen.insert(num);
+            max_sum = max(max_sum, curr_sum);
+        }
+        return max_sum;
+    }
+};
+
+class Solution
+{
     // Time limit excedded
 public:
     int maximumUniqueSubarray(vector<int> &nums)
