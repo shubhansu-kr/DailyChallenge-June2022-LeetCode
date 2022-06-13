9d772733e5f904f0a2fcf39d744cdadf922f2714
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-13 10:40:21 +0530

    Brute Force : TLE

---
+++ 12_Triangle.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/triangle/
+
+// Given a triangle array, return the minimum path sum from top to bottom.
+
+// For each step, you may move to an adjacent number of the row below.
+// More formally, if you are on index i on the current row, you may move
+// to either index i or index i + 1 on the next row.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // BruteForce : TLE
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        int sum = 0;
+        for (int i = 0; i < triangle.size(); ++i)
+        {
+            int subMin = INT_MAX;
+            for (int j = 0; j < triangle[i].size(); ++j)
+            {
+                subMin = min(triangle[i][j], subMin);
+            }
+            sum += subMin;
+        }
+        return sum;
+    }
+};
+
+int main()
+{
+    int x = -50;
+    for (int i = 0; i < 150; ++i)
+    {
+        cout << "[";
+        for (int j = 0; j < i ; ++j)
+        {
+            cout << x << ", ";
+            if (x > 100)
+                x = -100;
+            else
+                ++x;
+        }
+        cout << i << "],";
+    }
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
