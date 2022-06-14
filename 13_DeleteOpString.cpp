3d0e42b0b6ca75203be611a6cc7c07ed479e04bf
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-14 11:43:08 +0530

    Soluiton 2: Space optimisation

--- 13_DeleteOpString.cpp
+++ 13_DeleteOpString.cpp
@@ -8,6 +8,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Tabualtion : SPace optimisation
+public:
+    int minDistance(string word1, string word2)
+    {
+        vector<vector<int>> dp(2, vector<int>(size(word2) + 1));
+        for (int i = 0; i <= size(word1); i++)
+            for (int j = 0; j <= size(word2); j++)
+                if (!i || !j)
+                    dp[i & 1][j] = i + j;
+                else
+                    dp[i & 1][j] = word1[i - 1] == word2[j - 1] ? dp[(i - 1) & 1][j - 1] : 1 + min(dp[(i - 1) & 1][j], dp[i & 1][j - 1]);
+        return dp[size(word1) & 1][size(word2)];
+    }
+};
+
 class Solution1
 {
     // Tabualtion
