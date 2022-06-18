0a442853ead91b465a28f17b2930029d7808c109
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-18 11:41:44 +0530

    ShutDown

---
+++ 17_ShutDown.cpp
@@ -0,0 +1,17 @@
+// ShutDown due to riots
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    // Internet is taken down by the government of INDIA in 12 Districts of 
+    // Bihar(which includes mine), to supress the violent rebellion 'Agnipath'
+    // Due to the unavailability of Internet, Going to loose my streak :(
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
