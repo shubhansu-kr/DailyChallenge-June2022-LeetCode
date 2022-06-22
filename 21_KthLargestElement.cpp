966f09ce0454b851fd05219b74905a078406bd51
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-22 13:44:42 +0530

    Solution 9 : Heap Sort

--- 21_KthLargestElement.cpp
+++ 21_KthLargestElement.cpp
@@ -9,6 +9,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution9 {
+    // Heap Sort
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        buildMaxHeap(nums);
+        for (int i = 0; i < k - 1; i++) {
+            swap(nums[0], nums[--heapSize]);
+            maxHeapify(nums, 0);
+        }
+        return nums[0];
+    }
+private:
+    int heapSize;
+    
+    int left(int i) {
+        return (i << 1) + 1;
+    }
+    
+    int right(int i) {
+        return (i << 1) + 2;
+    }
+    
+    void maxHeapify(vector<int>& nums, int i) {
+        int largest = i, l = left(i), r = right(i);
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            maxHeapify(nums, largest);
+        }
+    }
+    
+    void buildMaxHeap(vector<int>& nums) {
+        heapSize = nums.size();
+        for (int i = (heapSize >> 1) - 1; i >= 0; i--) {
+            maxHeapify(nums, i);
+        }
+    }
+};
+
+class Solution8 {
+    // Partition
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1, kth;
+        while (true) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                kth = nums[idx];
+                break;
+            }
+            if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return kth;
+    }
+private:
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left], l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot) {
+                swap(nums[l++], nums[r--]);
+            }
+            if (nums[l] >= pivot) {
+                l++;
+            }
+            if (nums[r] <= pivot) {
+                r--;
+            }
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
+
 class Solution7
 {
     // MaxHeap : multiSet
