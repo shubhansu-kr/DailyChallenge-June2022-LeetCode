ab20906aa4948f25c96875d286a564887deb7bcd
shubhansu-kr <shubhansu2021@gmail.com>
2022-06-26 14:33:00 +0530

    SOlution 1: Recursion Optimised

--- 25_MaxPoints.cpp
+++ 25_MaxPoints.cpp
@@ -12,6 +12,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    // Recursion Improved
+public:
+    int maxScore(vector<int> &cardPoints, int k)
+    {
+        int points = 0, ans = INT_MIN, n = cardPoints.size();
+        return solve(cardPoints, k, 0, n - 1);
+    }
+    int solve(vector<int> &cardPoints, int k, int i, int j)
+    {
+        if (k == 0)
+        {
+            return 0;
+        }
+
+        // Pick left
+        int left = cardPoints[i] + solve(cardPoints, k - 1, i + 1, j);
+
+        // Pick Right
+        int right = cardPoints[j] + solve(cardPoints, k - 1, i, j - 1);
+
+        return max(left, right);
+    }
+};
+
 class Solution
 {
     // BruteForce: Recursion
@@ -41,11 +67,11 @@ public:
 
 int main()
 {
-    vector<int> cardPoints = {1, 9, 4, 2, 5, 3, 2};
-    int k = 2 ;
+    vector<int> cardPoints = {9,7,7,9,7,7,9};
+    int k = 7;
 
-    Solution Obj1;
-    cout << Obj1.maxScore(cardPoints, k) ;
+    Solution1 Obj1;
+    cout << Obj1.maxScore(cardPoints, k);
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
